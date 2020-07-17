5890094dac22e1515835daa6dd454b554c544b75
noober23 <66340165+noober23@users.noreply.github.com>
2020-07-16 20:53:26 -0400

    Update connect_db.php

--- db/connect_db.php
+++ db/connect_db.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=ipos','root','');
+    $pdo = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_d30f5750dcaa00c','b3db54c600f352','45b0b4df');
     //echo 'Connection Successfull';
 }catch(PDOException $error){
     echo $error->getmessage();
